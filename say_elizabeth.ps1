4d7fcd9974efb191c2587f8b9612f01db37f6c9c
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-08-07 22:20:00 +0530

    refactor code

--- say_elizabeth.ps1
+++
@@ -1,9 +0,0 @@
-
-Add-Type -AssemblyName System.Speech
-$synth = New-Object System.Speech.Synthesis.SpeechSynthesizer
-$voice = "Microsoft Aria Online (Natural) - English (United States)"
-$availableVoices = $synth.GetInstalledVoices().VoiceInfo.Name
-if ($availableVoices -contains $voice) {
-    $synth.SelectVoice($voice)
-}
-$synth.Speak("my, Welcome back, Gautam. Ready to code?")
